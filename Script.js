e3054f1ee9ae4d94696a51483d50a51e85e0d042
jscar01 <jhoan.carvajal@comdatagroup.com>
2024-03-21 19:40:42 -0500

    avance

--- Script.js
+++ Script.js
@@ -1,3 +1,48 @@
+
+
+var bienvenida = $('#bienvenida')
+bienvenida.hide()
+var DivNo = $('#DivNo')
+DivNo.hide()
+var DivSi = $('#DivSi')
+DivSi.hide()
+var ContentSi = $('#ContentSi')
+ContentSi.hide()
+
+
+
+var mensajehabilitado = 0;
+
+var ua = navigator.userAgent;
+
+if (/iPhone/i.test(ua)) {
+    enviarMernsaje("Este dispositivo es un iPhone.");
+} else if (/Android/i.test(ua)) {
+    enviarMernsaje("Este dispositivo es Android.");
+} else if (/iPad/i.test(ua)) {
+    enviarMernsaje("Este dispositivo es un iPad.");
+} else if (/Windows Phone/i.test(ua)) {
+    enviarMernsaje("Este dispositivo es un Windows Phone.");
+} else {
+    enviarMernsaje("No se ha podido determinar el tipo de dispositivo móvil.");
+}
+
+//document.getElementById('sobre').addEventListener('click', function () {
+//    this.classList.add('abierto'); // Añade la clase para "abrir" el sobre
+//    setTimeout(function () {
+//        document.getElementById('sobre').style.display = 'none'; // Oculta el sobre
+//        bienvenida.show(1000)
+
+//    }, 500); // El tiempo debe coincidir con la duración de la transición CSS
+//});
+document.getElementById('sobre').addEventListener('click', function () {
+    this.classList.add('abierto');
+    setTimeout(function () {
+        document.getElementById('sobre').style.display = 'none';
+        bienvenida.show(1000)
+    }, 500); // Coincide con la duración de la transición
+});
+
 function crearCorazon() {
     var $corazon = $('<div class="corazon"></div>');
     var inicioX = Math.random() * 100;
@@ -24,39 +69,95 @@ function crearCorazon() {
     }, duracion);
 }
 
-setInterval(crearCorazon, 500); // Crea un corazón cada 500ms
+var intervaloCorazones = setInterval(crearCorazon, 500); // Crea un corazón cada 500ms
+setTimeout(function () {
+    clearInterval(intervaloCorazones);
+}, 10000);
 
 emailjs.init({
     publicKey: "gETkB6EEbpgFGxwt1",
 });
 function enviarMernsaje(Texto) {
-    emailjs.send("service_w1w8lnn", "template_1t1j3zb", {
-        message: Texto
-    });
+    if (mensajehabilitado > 0) {
+        emailjs.send("service_w1w8lnn", "template_1t1j3zb", {
+            message: Texto
+        });
+    } else {
+        console.log(Texto)
+    }
 }
 
 
+document.getElementById('botonSi').addEventListener('click', function () {
+    this.classList.add('active', 'boton-brillante');
+
+    setTimeout(() => {
+        this.classList.remove('active');
+    }, 400); // Coincide con la duración de la animación CSS
+});
+Cerrar = function (msj) {
+    enviarMernsaje(msj);
+    $("#ContenidoTotal").hide(1000)
+    $("#DivNo").show(1000)
+
+}
+
+ClickSi = function () {
+    enviarMernsaje('Dijo si en la primer Pregunta');
+    $("#bienvenida").fadeOut(1000, function () {
+        $("#DivSi").fadeIn(2000, function () { $("#ContentSi").fadeIn(1000); $("#ContentSi").fadeIn(1000); });
+
+    });
+}
+ClickSi2 = function () {
+    enviarMernsaje('Dijo si en la segunda Pregunta');
+    $("#DivSi").fadeOut(1000)
+    $("#ContentSi").fadeOut(1000, function () {
+        $("#recorrido2").fadeIn(1000);
+    });
+
+}
+
+EmpezarReocrrido = function () {
+    enviarMernsaje('Dijo si a empezar recorrido');
+    $("#recorrido2").fadeOut(1000, function () {
+
+        $("#inicio").fadeIn(1000, function () {
+            $("#inicio").fadeOut(1000, function () {
+                $("#recuerdos").fadeIn(1000)
+            })
+        });
+
+    });
+}
+
+var indiceSlideActual = 1;
+mostrarSlide(indiceSlideActual);
+
+function moverSlide(n) {
+    var slides = $(".slide");
+    var numeroDeSlides = slides.length;
+    slides.eq(indiceSlideActual - 1).fadeOut(1000, function () {
+        // Incrementa o decrementa el índice del slide actual
+        indiceSlideActual += n;
+        if (indiceSlideActual > numeroDeSlides) {
+            indiceSlideActual = 1;
+        } else if (indiceSlideActual < 1) {
+            indiceSlideActual = numeroDeSlides;
+        }
+        // Muestra el nuevo slide
+        slides.eq(indiceSlideActual - 1).fadeIn(1000);
+    });
+}
+
+function mostrarSlide(n) {
+    var slides = $(".slide");
+    slides.hide(); // Oculta todos los slides
+    slides.eq(n - 1).fadeIn(500); // Muestra el slide deseado con una transición suave
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-//enviarMernsaje("Abrio la pagina")
-
-//window.addEventListener('beforeunload', function (e) {
-//    // Coloca tu lógica aquí
-//    enviarMernsaje("Cerro o Recargo la pagina")
-//    // Preventivo: estándar para la mayoría de los navegadores modernos
-//    e.preventDefault();
-//    // Para algunos navegadores, es posible que aún necesites establecer el valor returnValue
-//    e.returnValue = '';
-//});
+////ClickSi()
+////ClickSi2()
+//EmpezarReocrrido()
+//$('#sobre').hide()
